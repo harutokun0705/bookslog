eb7e7dde932ce7d0bd20ee4b6fd3f3c505dd80db
TERADA <teradaaa@teradaseiyanoMacBook-puro.local>
2021-09-10 15:48:55 +0900

    fix:layout

--- src/components/atoms/Button/Submitbutton.tsx
+++ src/components/atoms/Button/Submitbutton.tsx
@@ -4,16 +4,18 @@ import styled from 'styled-components';
 import { ButtonStyles } from '../../styles/Button/ButtonStyle'
 type ButtonProps = {
     children: string;
-    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+		disabled: boolean;
 }
 export const SubmitButton = (props: ButtonProps) => {
     const classes = ButtonStyles();
-    const { children, onClick } = props;
+    const { children, onClick, disabled } = props;
     return (
         <SButton
 					variant="contained"
 					className={`${classes.button} ${classes.textColor}`}
 					onClick={onClick}
+					disabled={disabled}
 					>
 					{children}
         </SButton>
--- src/components/organisms/InpurField.tsx
+++ src/components/organisms/InpurField.tsx
@@ -2,60 +2,113 @@ import styled from "styled-components"
 import { SubmitButton } from "../atoms/Button/Submitbutton"
 import { BooksNameField } from "../atoms/TextField/BooksNameField"
 import { CommentField } from "../atoms/TextField/CommentField"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+type submitContentsType = {
+	Name: string;
+	Comment: string
+}
 export const InputField = () => {
-    const [booksName,setBooksName] = useState('');
-    const [booksComment,setBooksComment] = useState('');
-		const label = {
-			name:"Books Name",
-			comment:"Books Comment"
+	const [booksName, setBooksName] = useState('');
+	const [booksComment, setBooksComment] = useState('');
+	const [buttonDisabled, setButtonDisabled] = useState(true);
+	const label = {
+		name: "Books Name",
+		comment: "Books Comment"
+	}
+	const [submitContents, setSubmitContents] = useState<submitContentsType[]>([]);
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const targetName = e.target.name;
+		const targetValue = e.target.value;
+		switch (targetName) {
+			case "booksName":
+				setBooksName(targetValue.trim());
+				break;
+			case "booksComment":
+				setBooksComment(targetValue.trim());
+				break;
 		}
-		const submitContents:any = [];
-		const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-			const targetName = e.target.name;
-        const targetValue = e.target.value;
-        switch(targetName) {
-					case "booksName":
-						setBooksName(targetValue);
-						break;
-					case "booksComment":
-						setBooksComment(targetValue);
-						break;
-        }
+		console.log(submitContents);
+	}
+	const handleSubmit = () => {
+		console.log(booksName, booksComment);
+		setSubmitContents([...submitContents, { Name: booksName, Comment: booksComment }])
+		setBooksName("");
+		setBooksComment("");
+	}
+	useEffect(() => {
+		if (booksName !== ""  && booksComment !== "" ) {
+			setButtonDisabled(false);
+		} else {
+			setButtonDisabled(true);
 		}
-		const handleSubmit = () => {
-			console.log(booksName,booksComment);
-			const inputContents = {booksName,booksComment};
-			submitContents.push(inputContents);
-			console.log(submitContents);
-			setBooksName("");
-			setBooksComment("");
-		}
-    return (
-        <SInputField>
-            <BooksNameField
-							name={"booksName"}
-							value={booksName}
-							label={label.name}
-							onChange={handleInputChange}
-            />
-            <CommentField
-							name={"booksComment"}
-							value={booksComment}
-							label={label.comment}
-							onChange={handleInputChange}
-						/>
-            <SubmitButton
-						onClick={handleSubmit}
-						>
-            	登録する
-            </SubmitButton>
-        </SInputField>
-    )
+	}, [booksName, booksComment])
+	return (
+		<SInputField>
+			<BooksNameField
+				name={"booksName"}
+				value={booksName}
+				label={label.name}
+				onChange={handleInputChange}
+			/>
+			<CommentField
+				name={"booksComment"}
+				value={booksComment}
+				label={label.comment}
+				onChange={handleInputChange}
+			/>
+			<SubmitButton
+				onClick={handleSubmit}
+				disabled={buttonDisabled}
+			>
+				登録する
+			</SubmitButton>
+			<SFlex>
+				{submitContents.map((item, i) =>
+					<SListWrappar>
+						<li key={i}><STitle>書籍名</STitle><br />{item.Name}</li>
+						<li key={i}><STitle>コメント<br /></STitle>{item.Comment}</li>
+					</SListWrappar>
+				)}
+			</SFlex>
+		</SInputField>
+	)
 }
 
 const SInputField = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
+`
+
+const SListWrappar = styled.div`
+width: 300px;
+height: 150px;
+overflow: scroll;
+word-wrap: break-word;
+border: solid 3px #2d76ca;
+border-radius: 5px;
+margin-top: 20px;
+/* margin-right: 68px; */
+padding: 10px;
+:nth-child(even) {
+	margin-right: 0;
+}
+li {
+	list-style: none;
+	color: #595757;
+}
+`
+
+const SFlex = styled.div`
+width: 720px;
+margin-top: 30px;
+display: flex;
+flex-wrap: wrap;
+justify-content: space-between;
+`
+
+const STitle = styled.span`
+font-weight: 500;
+font-size: large;
+color: #595757;
 `
